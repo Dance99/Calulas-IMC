826ae13b2146381e267c18861e69e78cdb789dbc
Daniela <nam991019@gmail.com>
2024-10-27 17:16:02 -0500

    categimc

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ const altura = parseFloat(prompt("ingrese su altura en m: "));
 if (IMC < 18.5){
     categoria = " Bajo peso";
 } else if (IMC < 24.9){
+    categoria = "Peso normal";
+} else if (IMC < 29.9){
+    categoria = "Sobre peso";
+} else {
+    categoria = "Obesidad";
+}
